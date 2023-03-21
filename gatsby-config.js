f6eacde547824a4a694d579a8ed900d6961c619b
him1994kush <him1994kush@gmail.com>
2023-03-21 23:57:38 +0530

    all files

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `paranoidBlog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: ['gatsby-plugin-postcss',],
+}
